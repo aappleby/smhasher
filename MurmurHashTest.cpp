8a4622ab8786317b950b4e63d4d53fa9b51c2da6
tanjent@gmail.com <tanjent@gmail.com@77a7d1d3-4c08-bdc2-d393-d5859734b01a>
2011-03-20 20:27:33 +0000

    Remove stub file

--- MurmurHashTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "pstdint.h"
-
-uint32_t MurmurHash1        ( const void * key, int len, uint32_t seed );
-uint32_t MurmurHash2        ( const void * key, int len, uint32_t seed );
-uint64_t MurmurHash64A      ( const void * key, int len, uint64_t seed );
-uint64_t MurmurHash64B      ( const void * key, int len, uint64_t seed );
-uint32_t MurmurHash2A       ( const void * key, int len, uint32_t seed );
-uint32_t MurmurHashNeutral2 ( const void * key, int len, uint32_t seed );
-uint32_t MurmurHashAligned2 ( const void * key, int len, uint32_t seed );
