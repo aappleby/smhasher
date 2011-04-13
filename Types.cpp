373ab1bdd592fa9f71f35951e0aea772c8e2fa01
tanjent@gmail.com <tanjent@gmail.com@77a7d1d3-4c08-bdc2-d393-d5859734b01a>
2011-04-13 23:36:49 +0000

    more size_t warnings

--- Types.cpp
+++ Types.cpp
@@ -43,7 +43,7 @@ bool isprime ( uint32_t x )
     199,211,223,227,229,233,239,241,251
   };
 
-  for(int i=0; i < sizeof(p)/sizeof(uint32_t); i++)
+  for(size_t i=0; i < sizeof(p)/sizeof(uint32_t); i++)
   { 
     if((x % p[i]) == 0)
     {
