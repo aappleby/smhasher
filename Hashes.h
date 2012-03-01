fe66abb07488bc634335d88ebe6646c9b7f28433
chandlerc@google.com <chandlerc@google.com@77a7d1d3-4c08-bdc2-d393-d5859734b01a>
2012-03-01 03:15:39 +0000

    Add Jenkins' Spooky hash to SMHasher's suite, in 32-bit, 64-bit, and 128-bit variations.

--- Hashes.h
+++ Hashes.h
@@ -36,6 +36,10 @@ void Crap8_test            ( const void * key, int len, uint32_t seed, void * ou
 void CityHash128_test      ( const void * key, int len, uint32_t seed, void * out );
 void CityHash64_test       ( const void * key, int len, uint32_t seed, void * out );
 
+void SpookyHash32_test     ( const void * key, int len, uint32_t seed, void * out );
+void SpookyHash64_test     ( const void * key, int len, uint32_t seed, void * out );
+void SpookyHash128_test    ( const void * key, int len, uint32_t seed, void * out );
+
 uint32_t MurmurOAAT ( const void * key, int len, uint32_t seed );
 
 //----------
