5b2536fa5d1f18f95cf654ab9b85f7f8f74e99da
tanjent@gmail.com <tanjent@gmail.com@77a7d1d3-4c08-bdc2-d393-d5859734b01a>
2012-05-11 04:59:07 +0000

    Merge in PMurHash by Shane Day, a MurmurHash3_x86_32 implementation that should work on virtually all platforms regardless of endian-ness or alignment issues.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "SpeedTest.h"
 #include "AvalancheTest.h"
 #include "DifferentialTest.h"
+#include "PMurHash.h"
 
 #include <stdio.h>
 #include <time.h>
@@ -78,6 +79,7 @@ HashInfo g_hashes[] =
   { MurmurHash3_x86_128, 128, 0xB3ECE62A, "Murmur3C",    "MurmurHash3 for x86, 128-bit" },
   { MurmurHash3_x64_128, 128, 0x6384BA69, "Murmur3F",    "MurmurHash3 for x64, 128-bit" },
 
+  { PMurHash32_test,      32, 0xB0F57EE3, "PMurHash32",  "Shane Day's portable-ized MurmurHash3 for x86, 32-bit." },
 };
 
 HashInfo * findHash ( const char * name )
