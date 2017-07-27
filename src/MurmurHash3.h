fce1f123225ae95aa97d94698cea7383dbdaf25f
nyavramov <nyavramov@gmail.com>
2017-07-27 11:57:28 -0400

    Fixes 2 errors: [ilink32 Error] Error: Unresolved external 'MurmurHash3_x86_128(const void *, int, unsigned int, void *)' referenced from MAIN.OBJ
    [ilink32 Error] Error: Unresolved external 'MurmurHash3_x64_128(const void *, const int, unsigned int, void *)' referenced from MAIN.OBJ

--- src/MurmurHash3.h
+++ src/MurmurHash3.h
@@ -28,9 +28,9 @@ typedef unsigned __int64 uint64_t;
 
 void MurmurHash3_x86_32  ( const void * key, int len, uint32_t seed, void * out );
 
-void MurmurHash3_x86_128 ( const void * key, int len, uint32_t seed, void * out );
+void MurmurHash3_x86_128 ( const void * key, const int len, uint32_t seed, void * out );
 
-void MurmurHash3_x64_128 ( const void * key, int len, uint32_t seed, void * out );
+void MurmurHash3_x64_128 ( const void * key, const int len, const uint32_t seed, void * out );
 
 //-----------------------------------------------------------------------------
 
