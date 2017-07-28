03d58ef365163e77a127f15272643b0d4d460a19
nyavramov <nyavramov@gmail.com>
2017-07-28 18:29:16 -0400

    Add bcc64.cmake

---
+++ src/bcc64.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_SYSTEM_NAME Win64)
+set(CMAKE_C_COMPILER C:/Program\ Files\ \(x86\)/Embarcadero/Studio/19.0/bin/bcc64.exe)
+set(CMAKE_CXX_COMPILER C:/Program\ Files\ \(x86\)/Embarcadero/Studio/19.0/bin/bcc64.exe)
