7d8b5ae14f4fe74922d284a2986908a98909e5d5
Nicolae Popovici <nicolae.o.popovici@intel.com>
2021-04-22 17:04:55 +0200

    optimized small key hashes by using bzhi instead of the switch construct

--- src/MurmurHash3.cpp
+++ src/MurmurHash3.cpp
@@ -6,6 +6,10 @@
 // algorithms are optimized for their respective platforms. You can still
 // compile and run any of them on any platform, but your performance with the
 // non-native version will be less than optimal.
+//
+//changes for MurmurHash3_x64_128 - small key hashes optimizations
+//Copyright (C) <2021> Intel Corporation
+//SPDX-License-Identifier: MIT
 
 #include "MurmurHash3.h"
 
@@ -258,6 +262,8 @@ void MurmurHash3_x64_128 ( const void * key, const int len,
   const uint8_t * data = (const uint8_t*)key;
   const int nblocks = len / 16;
 
+  unsigned int c = 0;
+
   uint64_t h1 = seed;
   uint64_t h2 = seed;
 
@@ -288,34 +294,23 @@ void MurmurHash3_x64_128 ( const void * key, const int len,
 
   const uint8_t * tail = (const uint8_t*)(data + nblocks*16);
 
-  uint64_t k1 = 0;
-  uint64_t k2 = 0;
+  uint64_t k1_02 = 0;
+  uint64_t k2_02 = 0;
 
-  switch(len & 15)
+  c = len & 15;
+  if (c!=0)
   {
-  case 15: k2 ^= ((uint64_t)tail[14]) << 48;
-  case 14: k2 ^= ((uint64_t)tail[13]) << 40;
-  case 13: k2 ^= ((uint64_t)tail[12]) << 32;
-  case 12: k2 ^= ((uint64_t)tail[11]) << 24;
-  case 11: k2 ^= ((uint64_t)tail[10]) << 16;
-  case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
-  case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
-           k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+	uint64_t pIntData = *(const uint64_t *)(data + nblocks * 16 + 8);
+	pIntData &= (1ULL<<(((c>>3)*(c%8))*8))-1;
+ 	k2_02 ^= pIntData; k2_02 *= c2; k2_02  = ROTL64(k2_02,33); k2_02 *= c1; h2 ^= k2_02;
 
-  case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
-  case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
-  case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
-  case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
-  case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
-  case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
-  case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
-  case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
-           k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
-  };
+	uint64_t pIntData2 = *(const uint64_t *)(data + nblocks * 16);
+	pIntData2 &= (1ULL<<(c*8))-1;
+ 	k1_02 ^= pIntData2; k1_02 *= c1; k1_02  = ROTL64(k1_02,31);k1_02 *= c2; h1 ^= k1_02;
+  }
 
   //----------
   // finalization
-
   h1 ^= len; h2 ^= len;
 
   h1 += h2;
